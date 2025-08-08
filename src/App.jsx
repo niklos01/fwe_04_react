5e6f957e56ccf3836e8d1e41f86620029a1cc35d
marvinxmo <109434966+dimipayet@users.noreply.github.com>
2025-08-08 10:13:57 +0200

    footer in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
-import { Routes, Route } from 'react-router-dom'
-import './App.css'
-import Home from './pages/Home'
-import Personen from './pages/Personen'
-import Header from './components/Header'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/footer';
+import Home from './pages/Home';
+import Personen from './pages/Personen';
 
 function App() {
-  return (
-    <div className="app">
-      <Header />
-      <main className="container mt-4">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/personen" element={<Personen />} />
-        </Routes>
-      </main>
-    </div>
-  )
+    return (
+        <div className="app">
+            <Header />
+            <main className="container mt-4">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/personen" element={<Personen />} />
+                </Routes>
+            </main>
+            <Footer />
+        </div>
+    );
 }
 
-export default App
+export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,6 @@ const Home = () => {
         <div className="home">
             <h1>Willkommen</h1>
             <p>Dies ist die Startseite unserer React-Anwendung.</p>
-            <Footer />
         </div>
     );
 };
