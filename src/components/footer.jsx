345fdd2dc03742e8a86e36ddbb248a07e2994c41
marvinxmo <109434966+dimipayet@users.noreply.github.com>
2025-08-08 10:09:01 +0200

    footer added2

---
+++ src/components/footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer className="bg-light text-center p-4 mt-auto">
+            <p className="mb-0">Uni Trier 2025, FWE Gruppe 4</p>
+        </footer>
+    );
+}
+
+export default Footer;
