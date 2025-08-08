921d51485df55db2fba40ccae28fd493fb4fc684
marvinxmo <109434966+dimipayet@users.noreply.github.com>
2025-08-08 10:08:46 +0200

    footer added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,11 @@
-import React from 'react'
-import Header from '../components/Header'
-
 const Home = () => {
-  return (
-    <div className="home">
-      <h1>Willkommen</h1>
-      <p>Dies ist die Startseite unserer React-Anwendung.</p>
-    </div>
-  )
-}
+    return (
+        <div className="home">
+            <h1>Willkommen</h1>
+            <p>Dies ist die Startseite unserer React-Anwendung.</p>
+            <Footer />
+        </div>
+    );
+};
 
-export default Home
+export default Home;
